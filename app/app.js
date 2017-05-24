4f948939b8a2cd6e3908e4413a336c182b51fca4
Aleksander Furtak <lorem_ipsum@mail.com>
2017-05-24 10:42:15 +0200

    volumeOfCylinder added

--- app/app.js
+++ app/app.js
@@ -1,14 +1,21 @@
 function getDescendingNumbers(start, stop)
 {
     if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
+        console.log("error")
         return false;
     }
-    var result = start;
-    for (var i = start - 1; i >= stop; i--) {
+       var result = start;
+       for (var i = start - 1; i >= stop; i--) {
         result = result + ' ' + i;
     }
-    return result;
+       return result;
 }
+
+function volumeOfCylinder(cylDiameter, cylHeight) {
+       var radius = cylDiameter / 2;
+       var volume = cylHeight * Math.PI * radius * radius;
+       return volume;
+}    ;
 
 module.exports = {
     getDescendingNumbers: getDescendingNumbers
--- test/app.spec.js
+++ test/app.spec.js
@@ -8,3 +8,8 @@ describe('getDescendingNumbers', function (){
         expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
     });
 });
+describe('volumeOfCylinder', function (){
+    it('should return volume of cylinder', function ()    {
+        expect(app.volumeOfCylinder(2, 2)).to.eql(6.283185307179586);
+    });
+});
